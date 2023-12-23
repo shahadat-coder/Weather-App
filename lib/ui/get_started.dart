f0d8dfc2b718c72979a497d452e6570507bea4d0
Md.Shahadat Hossain <112892447+shahadat349850@users.noreply.github.com>
2023-12-23 15:43:32 +0600

    Get started and welcom page has been added

---
+++ lib/ui/get_started.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/models/constants.dart';
+import 'package:weather_app/ui/welcome.dart';
+
+class GetStarted extends StatefulWidget {
+  const GetStarted({super.key});
+
+  @override
+  State<GetStarted> createState() => _GetStartedState();
+}
+
+class _GetStartedState extends State<GetStarted> {
+  @override
+  Widget build(BuildContext context) {
+    Constants myConstants = Constants();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+     body: Container(
+       width: size.width,
+       height: size.height,
+       color: myConstants.primaryColor.withOpacity(.5),
+       child: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         crossAxisAlignment: CrossAxisAlignment.center,
+         children: [
+           Image.asset('assets/get-started.png'),
+           const SizedBox(height: 30,),
+           GestureDetector(
+             onTap: (){
+               Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> const WelcomePage()));
+             },
+             child: Container(
+               height: 50,
+               width: size.width * 0.7,
+               decoration:  BoxDecoration(
+                 color: myConstants.primaryColor,
+                 borderRadius: const BorderRadius.all(Radius.circular(10))
+               ),
+               child: const Center(
+                 child: Text('Get started',style: TextStyle(color: Colors.white,fontSize: 18),),
+               ),
+             ),
+           )
+         ],
+       ),
+     ),
+
+    );
+  }
+}
